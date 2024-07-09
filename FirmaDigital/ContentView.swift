823dabbd5186afc4a3dc8f6cc8be6d9725094525
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-07-09 01:02:53 -0500

    Initial Commit

---
+++ FirmaDigital/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirmaDigital
+//
+//  Created by Brandon Luis Meneses Solorzano on 9/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirmaDigital/FirmaDigitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirmaDigitalApp.swift
+//  FirmaDigital
+//
+//  Created by Brandon Luis Meneses Solorzano on 9/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FirmaDigitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
